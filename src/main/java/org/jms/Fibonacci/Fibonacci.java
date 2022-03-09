7e44d434676d3e2241b0c50b4ab047a2ebd8d5b8
Javier Martín Sendra <javiervader15@gmail.com>
2022-03-09 20:38:42 +0100

    documentacion

--- src/main/java/org/jms/Fibonacci/Fibonacci.java
+++ src/main/java/org/jms/Fibonacci/Fibonacci.java
@@ -1,5 +1,11 @@
 package org.jms.Fibonacci;
-
+/*
+Javier Martín Sendra Software 3ºA
+--------------------------------------------------------
+Esta clase está compuesta por una función compute que nos devuelve la sucesión de Fibonacci
+para el número pasado por parámetro.
+--------------------------------------------------------
+ */
 public class Fibonacci {
 
 
--- src/test/java/org/jms/Fibonacci/FibonacciTest.java
+++ src/test/java/org/jms/Fibonacci/FibonacciTest.java
@@ -1,5 +1,16 @@
 package org.jms.Fibonacci;
-
+/*
+Javier Martín Sendra Software 3ºA
+--------------------------------------------------------
+Esta clase está compuesta por diferentes test para comprobar el buen funcionamiento del método compute de la clase
+Fibonacci.
+Comprobamos los siguientes casos de prueba con diferentes asserts:
+-Caso número 0
+-Caso número 1
+-Caso cualquier numero mayor que uno
+-Caso numero negativo(salta excepcion)
+--------------------------------------------------------
+ */
 public class FibonacciTest {
 
 
